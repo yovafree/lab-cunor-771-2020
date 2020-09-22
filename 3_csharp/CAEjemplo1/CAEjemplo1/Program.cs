7572215c0e4e46cc697dd6c91152038f1da8556d
Yova de León <erwingeo1021@gmail.com>
2020-09-22 10:54:26 -0600

    Se agregan ejemplos de C#

---
+++ 3_csharp/CAEjemplo1/CAEjemplo1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CAEjemplo1
+{
+    class Program
+    {
+        static int num1=5;
+
+        /***
+         * Esto es un comentario de documentación 
+         * nos ayuda a mejorar la visibilidad de una descripción.
+         */
+        static void Main(string[] args)
+        {
+            num1 = -3;
+            num1 = 0;
+
+            #region "Uso de IF"
+            // IF
+            if (num1 > 0)
+            {
+                Console.WriteLine("Es positivo");
+            }
+            else if (num1 == 0) {
+                Console.WriteLine("Es cero");
+            }
+            else
+            {
+                Console.WriteLine("Es negativo");
+            }
+            #endregion
+
+            num1 = 10;
+            #region "Uso de Switch"
+            switch (num1) {
+                case 0:
+                    Console.WriteLine("El valor es 0");
+                    break;
+                case 1:
+                    Console.WriteLine("El valor es 1");
+                    break;
+                case 2:
+                    Console.WriteLine("El valor es 2");
+                    break;
+                default:
+                    Console.WriteLine("El valor es default");
+                    break;
+            }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
