6a3d5f06e4feb885b6037c75123f63e0dfebe59b
Yova de León <erwingeo1021@gmail.com>
2020-10-27 10:46:43 -0600

    Sesiones y autenticación con .NET MVC

---
+++ 3_csharp/slnMVC/mvcEjemplo3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcEjemplo3.MvcApplication" Language="C#" %>
---
+++ 3_csharp/slnMVC/mvcEjemplo3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcEjemplo3.Startup))]
+namespace mvcEjemplo3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
